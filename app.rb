c47d8ce596c2ec04fd95826484dc7f915986d602
Travis Hooper <travis.d.hooper@gmail.com>
2020-09-15 11:24:41 -0700

    Working API

--- app.rb
+++ app.rb
@@ -1,18 +1,13 @@
 # myapp.rb
 require 'dotenv/load'
 require 'sinatra'
-require "sinatra/reloader"
+require 'sinatra/reloader' if development?
 require 'faraday'
 
-API_HOST = "https://api.yelp.com"
-SEARCH_PATH = "/v3/businesses/search"
-BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
-
-
-DEFAULT_BUSINESS_ID = "yelp-san-francisco"
-DEFAULT_TERM = "dinner"
-DEFAULT_LOCATION = "San Francisco, CA"
-SEARCH_LIMIT = 5
+API_HOST            = 'https://api.yelp.com'
+SEARCH_PATH         = '/v3/businesses/search'
+DEFAULT_TERM        = 'dinner'
+DEFAULT_LOCATION    = 'Austin, TX'
 
 get '/' do
   url = "#{API_HOST}#{SEARCH_PATH}"
@@ -25,12 +20,4 @@ get '/' do
   end
 
   resp.body
-end
-
-get '/new_route' do
-  { message: "NEW ROUTE #{params['fooobaaar']}" }.to_json
-end
-
-get '/new_route/:id' do
-  { message: "NEW ROUTE WITH ID of: #{params['id']}" }.to_json
 end
