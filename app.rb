aa5efe7b3d1b5b61796bdd53e443bb1d14b4aff0
Travis Hooper <travis.d.hooper@gmail.com>
2020-09-14 18:56:56 -0700

    Adds yelp API

--- app.rb
+++ app.rb
@@ -1,9 +1,29 @@
 # myapp.rb
 require 'sinatra'
 require "sinatra/reloader"
+require 'faraday'
+
+API_HOST = "https://api.yelp.com"
+SEARCH_PATH = "/v3/businesses/search"
+BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
+
+
+DEFAULT_BUSINESS_ID = "yelp-san-francisco"
+DEFAULT_TERM = "dinner"
+DEFAULT_LOCATION = "San Francisco, CA"
+SEARCH_LIMIT = 5
 
 get '/' do
-  { message: 'I AM A SERVER' }.to_json
+  url = "#{API_HOST}#{SEARCH_PATH}"
+
+  resp = Faraday.get(url) do |req|
+    req.params['term'] = params[:term] || DEFAULT_TERM
+    req.params['location'] = params[:location] || DEFAULT_LOCATION
+    req.headers['Content-Type'] = 'application/json'
+    req.headers['Authorization'] = "Bearer #{ENV['API_KEY']}"
+  end
+
+  resp.body
 end
 
 get '/new_route' do
